5264f5e09242db1be261fd5163b1db4fd01aa5f1
dangchien2003 <chienboy03@gmail.com>
2024-04-28 23:27:38 +0700

    Update ConfigThread.java

--- src/main/java/com/automaticparking/config/ConfigThread.java
+++ src/main/java/com/automaticparking/config/ConfigThread.java
@@ -20,7 +20,7 @@ public class ConfigThread {
         executor.setQueueCapacity(100);
         executor.setThreadNamePrefix("Async-");
         executor.initialize();
-        System.out.println("Thread OK max 20");
+        System.out.println("Thread OK");
         return executor;
     }
 }
