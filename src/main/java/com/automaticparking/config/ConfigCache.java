28df984e68584abfbd077f503cf59cfad0162662
dangchien2003 <chienboy03@gmail.com>
2024-05-01 15:18:03 +0700

    Update ConfigCache.java

--- src/main/java/com/automaticparking/config/ConfigCache.java
+++ src/main/java/com/automaticparking/config/ConfigCache.java
@@ -17,7 +17,7 @@ public class ConfigCache {
             Cache<String, Object> cache = Caffeine.newBuilder()
 //                    .maximumSize(Integer.parseInt(CustomDotENV.get("MAX_CACHE"))) // Số lượng mục tối đa trong cache
                     .expireAfterWrite(Long.parseLong(CustomDotENV.get("EXPIRE_CACHE_SECOND")), TimeUnit.SECONDS) // Thời gian sống của mỗi mục trong cache tính từ lúc ghi
-                    .expireAfterAccess(Long.parseLong(CustomDotENV.get("EXPIRE_CACHE_SECOND")), TimeUnit.SECONDS) // Thời gian sống của mỗi mục trong cache tính từ lần cuối truy cập cache
+//                    .expireAfterAccess(Long.parseLong(CustomDotENV.get("EXPIRE_CACHE_SECOND")), TimeUnit.SECONDS) // Thời gian sống của mỗi mục trong cache tính từ lần cuối truy cập cache
                     .build();
             System.out.println("Config cache OK");
             return cache;
