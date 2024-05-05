1f071e62993f9c0e0a009d3b00e1285b339d1b89
dangchien2003 <chienboy03@gmail.com>
2024-05-06 01:07:12 +0700

    fix cors

---
+++ src/main/java/com/automaticparking/config/ConfigCORS.java
@@ -0,0 +1,26 @@
+package com.automaticparking.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class ConfigCORS implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("https://localhost:3000", "http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders("Authorization")
+                .allowPrivateNetwork(true)
+                .maxAge(60);
+
+    }
+
+}
