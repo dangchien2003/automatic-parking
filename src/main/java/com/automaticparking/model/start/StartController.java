c29ba6b38e94e9962345f5877b2ab266274bd810
dangchien2003 <chienboy03@gmail.com>
2024-05-27 01:30:35 +0700

    api hello

---
+++ src/main/java/com/automaticparking/model/start/StartController.java
@@ -0,0 +1,20 @@
+package com.automaticparking.model.start;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("start")
+public class StartController {
+    @GetMapping("hello")
+    ResponseEntity<?> hello(){
+        Map<String, String> hello = new HashMap<>();
+        hello.put("start", "hello");
+        return ResponseEntity.ok(hello);
+    }
+}
