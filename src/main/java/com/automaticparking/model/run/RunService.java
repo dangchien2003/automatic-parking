f40c50b2f05e8491b636a2bfbf2eba20e5ae306b
dangchien2003 <chienboy03@gmail.com>
2024-07-02 02:08:38 +0700

    update run

--- src/main/java/com/automaticparking/model/run/RunService.java
+++ src/main/java/com/automaticparking/model/run/RunService.java
@@ -1,25 +1,55 @@
 package com.automaticparking.model.run;
 
+import com.automaticparking.model.cache.CacheService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.*;
+import java.net.URL;
 import java.util.concurrent.Executor;
 
 @Service
 public class RunService {
     private Executor asyncExecutor;
+    private CacheService cacheService;
 
     @Autowired
-    public RunService(Executor asyncExecutor) {
+    public RunService(Executor asyncExecutor, CacheService cacheService) {
         this.asyncExecutor = asyncExecutor;
+        this.cacheService = cacheService;
     }
 
     @Async
     public void run() {
-        System.out.println("acs");
+
+        File file = new File("run.txt");
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
+
+        try {
+            // Kiểm tra nếu file không tồn tại hoặc file trống
+            if (!file.exists() || file.length() == 0) {
+                writer = new BufferedWriter(new FileWriter(file));
+                writer.write("running");
+                System.out.println("starting...");
+            } else {
+                System.out.println("runed");
+                return;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) reader.close();
+                if (writer != null) writer.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
         asyncExecutor.execute(() -> {
             while (true) {
                 RestTemplate restTemplate = new RestTemplate();
