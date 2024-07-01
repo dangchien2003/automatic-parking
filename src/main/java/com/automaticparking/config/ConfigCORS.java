c31065e22afbbc1a633300740bea4c8ce6e16a8e
dangchien2003 <chienboy03@gmail.com>
2024-07-01 22:33:54 +0700

    Update ConfigCORS.java

--- src/main/java/com/automaticparking/config/ConfigCORS.java
+++ src/main/java/com/automaticparking/config/ConfigCORS.java
@@ -15,7 +15,7 @@ public class ConfigCORS implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://localhost:3000", "http://localhost:3000", "https://fe-parking.onrender.com", "https://feparking.com:3000", "http://feparking.com:3000", "https://child1.feparking.com:3000", "http://child1.feparking.com:3000", "https://bot-app-3wke.onrender.com")
+                .allowedOrigins("https://fe-parking.vercel.app", "https://fe-parking-zcc6rh1xd-dangchien2003s-projects.vercel.app","https://localhost:3000", "http://localhost:3000", "https://fe-parking.onrender.com", "https://feparking.com:3000", "http://feparking.com:3000", "https://child1.feparking.com:3000", "http://child1.feparking.com:3000", "https://bot-app-3wke.onrender.com")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
