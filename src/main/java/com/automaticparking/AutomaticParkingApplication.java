3e698c679d06952bef919f06d91eb2836dd42bd5
dangchien2003 <chienboy03@gmail.com>
2024-07-22 08:57:39 +0700

    update config

--- src/main/java/com/automaticparking/AutomaticParkingApplication.java
+++ src/main/java/com/automaticparking/AutomaticParkingApplication.java
@@ -1,6 +1,5 @@
 package com.automaticparking;
 
-import lombok.extern.log4j.Log4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
